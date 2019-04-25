52bc4c9e71fa2c75eb24dd24eb7e46645c03f8e4
kim12355 <kloveshiro@gmail.com>
2019-04-25 14:39:40 +0700

    get list Accom
    sdfd

---
+++ ScrumProject/ScrumProject/BAL/HouseBAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ScrumProject.Models;
+
+namespace ScrumProject.BAL
+{
+    public class HouseBAL
+    {
+        private Scrum_StudentAccomodationContext context;
+
+        public HouseBAL(Scrum_StudentAccomodationContext context)
+        {
+            this.context = context;
+        }
+
+        public List<House> getAllHouse()
+        {
+            return context.House.ToList();
+        }
+    }
+}
