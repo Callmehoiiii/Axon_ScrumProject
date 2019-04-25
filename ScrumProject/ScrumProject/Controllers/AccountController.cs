546581ba2dac1318412f53a69795865b54a5ab51
Callmehoiiii <thedangthanhh@gmail.com>
2019-04-25 15:14:08 +0700

    a

--- ScrumProject/ScrumProject/Controllers/AccountController.cs
+++ ScrumProject/ScrumProject/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace ScrumProject.Controllers
     {
         public IActionResult Login()
         {
-
+            return null;
         }
     }
 }
